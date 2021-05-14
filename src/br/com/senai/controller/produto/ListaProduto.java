afe80c330bb31154db197eba2cceed604556bfc0
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-05-14 10:52:02 -0300

    Refatoração para verificar se possui dados cadastrados no BD

--- src/br/com/senai/controller/produto/ListaProduto.java
+++ src/br/com/senai/controller/produto/ListaProduto.java
@@ -1,5 +1,6 @@
 package br.com.senai.controller.produto;
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import br.com.dao.DataBaseConnection;
@@ -23,8 +24,18 @@ public class ListaProduto {
 			String sql ="Select * from produto";
 			preparedStatement = connection.prepareStatement(sql);
 			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if(!resultSet.next()) {
+				System.out.println("Não possui dados cadastrados.");
+				return null;
+			}
+			
 			System.out.println("--- PRODUTOS CADASTRADOS ---");
 			System.out.printf("| %2s | %15s | %7s | %4s | %9s|\n", "ID", "Produto", " Preço ", " Qtd ", "R$ Total");
+			
+			
+			resultSet.previous();
+			
 			while(resultSet.next()) {
 				System.out.printf("| %2s | %15s | %7s | %4s | %9s |\n",
 						resultSet.getInt("codigo"),
