86b0b771f857fa135a1ee4635c60b7206249f16d
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-05-17 11:46:38 -0300

    Criação de classe para organização do carrinho

---
+++ src/br/com/senai/controller/carrinho/VisualizaCarrinho.java
@@ -0,0 +1,47 @@
+package br.com.senai.controller.carrinho;
+
+import java.util.List;
+import java.util.Scanner;
+
+import br.com.senai.model.Carrinho;
+import br.com.senai.model.ProdutoModel;
+
+public class VisualizaCarrinho {
+
+	Scanner scanner = new Scanner(System.in);
+	
+	public void verCarrinho(List<Carrinho> listaCarrinho, List<ProdutoModel> produtos,String cliente) {
+
+		ListaCarrinho listaCarrinhoItens = new ListaCarrinho();
+		AdicionaItemNoCarrinho addCarrinho = new AdicionaItemNoCarrinho();
+		RemoveCarrinho removeCarrinho = new RemoveCarrinho();
+		FinalizaCompra finalizaCompra = new FinalizaCompra();
+		
+		listaCarrinhoItens.listarCarrinho();
+		
+		System.out.println("--- CARRINHO ---");
+		System.out.println("1) Adicionar itens");
+		System.out.println("2) Remover itens");
+		System.out.println("3) Finalizar compra");
+		System.out.println("4) Sair do carrinho");
+		int opcaoCarrinho = scanner.nextInt();
+
+		switch (opcaoCarrinho) {
+		case 1:
+			addCarrinho.adicionarCarrinho();
+			break;
+		case 2:
+			removeCarrinho.removerCarrinho(listaCarrinho, produtos, cliente);
+			break;
+		case 3:
+			finalizaCompra.finalizarCompra(listaCarrinho, produtos, cliente);
+			break;
+		case 4:
+			
+			break;
+			default:
+				System.out.println("Opção inexistente");
+			break;
+		}
+	}
+}
