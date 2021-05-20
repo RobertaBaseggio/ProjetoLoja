2ea18a2f1a2c8f8fdc4ab2c7c4bf66c744070d7a
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-05-20 13:28:02 -0300

    edição do codigo para funcionamento da função remover

--- src/br/com/senai/controller/carrinho/RemoveCarrinho.java
+++ src/br/com/senai/controller/carrinho/RemoveCarrinho.java
@@ -1,8 +1,11 @@
 package br.com.senai.controller.carrinho;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
+import br.com.dao.DataBaseConnection;
 import br.com.senai.model.Carrinho;
 import br.com.senai.model.ProdutoModel;
 
@@ -11,28 +14,95 @@ public class RemoveCarrinho {
 	Scanner scanner = new Scanner(System.in);
 	
 	VisualizaCarrinho visualizaCarrinho = new VisualizaCarrinho();
+	ProdutoModel produto = new ProdutoModel();
+	ResultSet resultSet;
 	
+	private Connection connection;
 	
-	public void removerCarrinho(List<Carrinho> listaCarrinho, List<ProdutoModel> produtos, String cliente) {
+	public RemoveCarrinho() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
+	
+	public boolean verifivaSeExisteProdutoNoCarrinho(int idDoProduto) {
+		PreparedStatement preparedStatement;
+
+		try {
+			String sql = "Select * from itensNoCarrinho where codigo = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idDoProduto);
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (!resultSet.next()) {
+				System.out.println("Este produto não existe");
+				return false;
+			} else {
+				return true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+			return false;
+		}
+	}
+	
+	public void removerCarrinho( String cliente) {
 		
-		ListaCarrinho listaCarrinhoItens = new ListaCarrinho();
+		PreparedStatement preparedStatement;
 		
-		listaCarrinhoItens.listarCarrinho();
+		ListaCarrinho listaCarrinho = new ListaCarrinho();
+		Carrinho carrinho = new Carrinho();
+		
 		
 		System.out.println("--- REMOVER ---");
-		System.out.print("Insira o Id do Produto: ");
-		int idDoProdutoRemov = scanner.nextInt();
 		
-		
-		
-		if (idDoProdutoRemov > listaCarrinho.size()) {
-			System.out.println("Esse produto não existe no seu carrinho");
-			visualizaCarrinho.verCarrinho(listaCarrinho, produtos, cliente);
+		if (listaCarrinho.listarCarrinho() == null) {
+			System.out.println("Não existem produtos cadastrados");
 			return;
+		}
+		
+		
+		System.out.print("Insira o Id do Produto: ");
+		int idDoProduto = scanner.nextInt();
+
+		
+		try {
+			
+			if (!verifivaSeExisteProdutoNoCarrinho(idDoProduto)) {
+				return;
+			}
+			
+			
+			String sql = "delete from itensNoCarrinho where codigo = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, idDoProduto);
+			preparedStatement.execute();
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Não foi possível excluir essa informação!");
+		}
+		
+		try {
+			
+			
+			
+			String sql2 ="update produto set quantidadeEmEstoque = quantidade em estoque + ? where codigo = ? ";
+			preparedStatement = connection.prepareStatement(sql2);
+			
+			preparedStatement.setInt(1, carrinho.getQuantidadeDoProduto());
+			preparedStatement.setInt(2, idDoProduto);
+			preparedStatement.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	
 			
 		}
 
-		listaCarrinho.remove(idDoProdutoRemov - 1);
-		visualizaCarrinho.verCarrinho(listaCarrinho, produtos, cliente);
-	}
+	
 }
