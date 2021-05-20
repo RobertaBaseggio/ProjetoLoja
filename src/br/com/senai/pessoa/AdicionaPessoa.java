af6bb3f09bc03be525d6b2dfd96089a6938efde3
Roberta Baseggio <roberta_polez@estudante.sc.senai.br>
2021-05-20 13:25:13 -0300

    refatoração da classe pessoa para conpatibilidade com o BD

--- src/br/com/senai/pessoa/AdicionaPessoa.java
+++ src/br/com/senai/pessoa/AdicionaPessoa.java
@@ -1,16 +1,39 @@
 package br.com.senai.pessoa;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Scanner;
 
-public class AdicionaPessoa {
+import br.com.dao.DataBaseConnection;
 
-public String definirCliente() {
-		
-	@SuppressWarnings("resource")
+public class AdicionaPessoa {
+	
+	private Connection connection;
+	
+	public AdicionaPessoa() {
+		connection = DataBaseConnection.getInstance().getConnection();
+
+	}
+
+	public String definirCliente() {		
+	
 	Scanner scanner = new Scanner(System.in);
 	
-		System.out.println("Infome o nome do cliente: ");
+	PreparedStatement preparedStatement;
+	
+	System.out.println("Infome o nome do cliente: ");
 		String nome = scanner.next();
+		
+		try {
+			
+			String sql ="insert into cliente values (null, ?)";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, nome);
+			preparedStatement.execute();
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		return nome;
 	}
